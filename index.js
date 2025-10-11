ddd2921f24acaba6cee3e43c9f127c2d02673efb
Shivam Patidar <98587143+Shivu7889@users.noreply.github.com>
2025-10-11 06:03:32 +0530

    Update CORS

--- index.js
+++ index.js
@@ -51,17 +51,7 @@ const allowedOrigins = [
 // Middleware
 
 app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `The CORS policy for this site does not allow access from ${origin}`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true,
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
@@ -594,3 +584,4 @@ app.listen(PORT, () => {
 });
 
 module.exports = app;
+
